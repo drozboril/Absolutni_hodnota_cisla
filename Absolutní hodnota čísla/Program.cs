70e359be9f574de1a321aeed449b2b855a5d06eb
drozboril <rozborildavid77@gmail.com>
2022-11-24 18:28:59 +0100

    Uživatel zadá číslo , program prostřednictví samostatné metody vypíše jeho absolutní hodnotu

---
+++ Absolutní hodnota čísla/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absolutní_hodnota_čísla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Absolutní hodnota čísla\nZadejte číslo");
+            int cislo1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Absolutní hodnota zadaného čísla je {0}", Abs(cislo1));
+            Console.ReadLine();
+        }
+        static int Abs(int cislo1)
+        {
+            int vypocet = Math.Abs(cislo1);
+            return vypocet;
+        }
+    }
+}
